1b9e9f8c4fe32c26557f4f051bc047ac109d920e
Stefano-Scozziero <scozzierostefano@gmail.com>
2024-01-18 20:48:25 -0300

    Pal profe

--- App.js
+++ App.js
@@ -1,49 +1,46 @@
-import React, { useState } from 'react';
-import { Button, TextInput, View, StyleSheet, Text, ImageBackground, Image } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { Button, TextInput, View, StyleSheet, Text, ImageBackground, Image } from 'react-native'
 
-export default function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = () => {
-    // Aquí puedes manejar la lógica de inicio de sesión
-    console.log(`Iniciando sesión con el usuario: ${username}`);
-  };
+const App = ()  => {
 
   return (
-    <ImageBackground source={require('./images/fondodefinitivo.png')} style={styles.container}>
-      <Image source={require('./images/logo.png')} style={styles.logo} />
+    <ImageBackground source={require('./assets/fondodefinitivo.png')} style={styles.container}>
+      <Image source={require('./assets/logo.png')} style={styles.logo} />
+      <View style={styles.text}>
+        <Text>¡Bienvenido a PRODESCO!</Text>
+      </View>
+      
       <TextInput
-        value={username}
-        onChangeText={setUsername}
         placeholder="Nombre de usuario"
         style={styles.input}
+        selectionColor={'#9ed7ea'}
       />
       <TextInput
-        value={password}
-        onChangeText={setPassword}
         placeholder="Contraseña"
         secureTextEntry
+        selectionColor={'#9ed7ea'}
         style={styles.input}
       />
-      <Button title="Iniciar sesión" onPress={handleLogin}/>
-      <StatusBar style="auto" />
+      <Button title="Iniciar sesión" style={styles.button}/>
     </ImageBackground>
   );
+
+  
 }
+
+export default  App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fffff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   input: {
     height: 40,
     borderColor: 'gray',
-    borderWidth: 1,
+    borderBottomWidth: 1,
+    color: 'white',
     marginBottom: 20,
     width: '80%',
     padding: 10,
@@ -53,7 +50,8 @@ const styles = StyleSheet.create({
     height: 150,
     marginBottom: 20,
     resizeMode: 'contain',
-    
+  },
+  text: {
+    marginBottom: 20,
   }
-  
 });
